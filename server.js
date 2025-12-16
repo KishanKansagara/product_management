80ff65835e02ecbcb20bcabab6dd51103e839950
kingston_node <you@example.com>
2025-12-16 12:40:32 +0530

    update node_env envirement

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({
         success: false,
         message: 'Something went wrong!',
-        error: process.env.NODE_ENV === 'development' ? err.message : undefined
+        error: process.env.NODE_ENV === 'localhost' ? err.message : undefined
     });
 });
 
